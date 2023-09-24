57e49ba6cd144a88ced5f1a38e00fc9b46042a99
boazglick <95533522+boazglick@users.noreply.github.com>
2023-09-24 09:06:21 +0300

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -3,21 +3,21 @@ module.exports = {
 
   postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-  siteTitleMeta: "american-airlines-tickets", // This allows an alternative site title for meta data for pages.
+  siteTitleMeta: "american-tickets", // This allows an alternative site title for meta data for pages.
   siteDescriptionMeta:
-    "american-airlines-tickets", // This allows an alternative site description for meta data for pages.
+    "american-tickets", // This allows an alternative site description for meta data for pages.
 
   shareImageWidth: 1000, // Change to the width of your default share image
   shareImageHeight: 523, // Change to the height of your default share image
 
-  shortTitle: "american-airlines-tickets", // Used for App manifest e.g. Mobile Home Screen
+  shortTitle: "american-tickets", // Used for App manifest e.g. Mobile Home Screen
   siteIcon: "favicon.png", // Logo in /static dir used for SEO, RSS, and App manifest
   backgroundColor: "#e9e9e9", // Used for Offline Manifest
   themeColor: "#15171A", // Used for Offline Manifest
   apiUrl: "https://wordpress.theasdfghjkl.com",
   subscribeWidget: {
     visible: false,
-    title: "american-airlines-tickets",
+    title: "american-tickets",
     helpText: "הישארו מעודכנים",
     successMessage: "תודה ההודעה נשלחה בהצלחה",
   },
